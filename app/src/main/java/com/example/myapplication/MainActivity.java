183d626f7c51a555443a94e5b43ceab9661e0848
ramzan <ramzanelayodath@gmail.com>
2024-08-14 14:09:39 +0530

    #100 fix

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
              }
             //Log.e("MYJSON",name);
 
-            System.out.println("develop");
+            System.out.println(" #develop");
 
         } catch (JSONException e) {
             throw new RuntimeException(e);
